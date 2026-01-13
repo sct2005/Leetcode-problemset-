d352edc3b41eba6645f71c44fd4f445e829fafcf
Samuel Taylor-KIng <sct215@exeter.ac.uk>
2026-01-13 12:11:49 +0000

    Update palidrome.py

--- math/palidrome.py
+++ math/palidrome.py
@@ -1,1 +1,22 @@
 
+import math
+
+class Solution(object):
+
+
+
+
+    def palidrome(x): 
+        if x < 0:
+            return False
+        original = x 
+        reversed_num = 0
+        while x > 0:
+            reversed_num = reversed_num * 10 +  (x % 10)
+            x = x // 10 
+        if reversed_num == original:
+            print("the number is a palidrome")
+        else:
+            print("the number is not an palidrome")
+
+    palidrome(121)
