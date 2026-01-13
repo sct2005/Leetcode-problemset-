69b9caf2d12bf9566d697285d7933f8be6c28e89
Samuel Taylor-KIng <sct215@exeter.ac.uk>
2026-01-13 11:46:55 +0000

    Update two_some.py

--- arrays/two_some.py
+++ arrays/two_some.py
@@ -1,1 +1,11 @@
 
+class Solution(object):
+    nums = [1,2,3,4,5,6,7,8,9]
+    def twosum(nums, target):
+        for i in range(len(nums)):
+            for k in range(i + 1, len(nums)):
+                if nums[i] + nums[k] == target:
+                    print(nums[i], nums[k])
+                    return
+
+    twosum(nums, 10)
