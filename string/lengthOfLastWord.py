84b37918b1da21b0f206d899c14e69742dfffef0
Samuel Taylor-KIng <sct215@exeter.ac.uk>
2026-01-14 12:04:51 +0000

    Update lengthOfLastWord.py

--- string/lengthOfLastWord.py
+++ string/lengthOfLastWord.py
@@ -1,1 +1,17 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
 
+
+        
+        s_temp = ''
+        for i in s:
+            if i == " ":
+                s_temp = ''
+            else:
+                s_temp += i
+                
+
+        print(len(s_temp))
+
+
+Solution().lengthOfLastWord("hello sam its callum")
