ec67f224f0d5b6277817a5b497eabceb77e3e517
Samuel Taylor-KIng <sct215@exeter.ac.uk>
2026-01-25 19:38:44 +0000

    Update 2273.py

--- arrays/2273.py
+++ arrays/2273.py
@@ -1,1 +1,24 @@
 
+words = ["abba","baba","bbaa","cd","cd"]
+
+
+
+def array_alter(words):
+    for word in words:
+            
+
+        if sorted(word) in words:
+            words.remove(word)
+            print("a")
+        else:
+            continue
+
+
+array_alter(words)
+
+
+            
+
+
+        
+    
